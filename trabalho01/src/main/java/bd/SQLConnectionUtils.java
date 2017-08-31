4509f95ad8977d44c95c171c4626932ac5998306
igor.soliveira11 <igor.soliveira11@CAS555504W0706.senacsp.edu.br>
2017-08-30 21:51:56 -0300

    SQLConnectionUtils

--- trabalho01/src/main/java/bd/SQLConnectionUtils.java
+++ trabalho01/src/main/java/bd/SQLConnectionUtils.java
@@ -5,10 +5,38 @@
  */
 package bd;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 /**
  *
- * @author ricardo.pleite
+ * @author igor.oliveira
  */
 class SQLConnectionUtils {
+     //Obtém uma conexão do banco de dados
+    public static Connection getConnection() {
+        //Conexão para abertura e fechamento
+        Connection connection = null;
+        try {
+            //Só tenta abrir uma conexão se não existir ou estiver fechada            
+            //Endereço de conexão com o banco de dados
+            String dbURL = MainConstants.DB_ADDRESS;
+            //Propriedades para armazenamento de usuário e senha
+            Properties properties = new Properties();
+            properties.put("user", MainConstants.DB_USER);
+            properties.put("password", MainConstants.DB_PASS);
+            //Realiza a conexão com o banco
+            connection = DriverManager.getConnection(dbURL, properties);
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        //Retorna a conexão
+        return connection;
+    }
+    
     
 }
